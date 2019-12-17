dc3a8c2ff8039ced734b08c4ffb683275206ba1c
Sujan Koju <myselfsujan67@gmail.com>
2019-12-17 20:21:27 +0545

    Add files via upload

---
+++ src/controllers/HomeController.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	//Request Mapping for Main Interface Starts
+	/* Koju's Protion Starts */
+	@RequestMapping(value="/")
+	public ModelAndView indexPage() {
+		return new ModelAndView("index","page","mainBody");
+	}
+	@RequestMapping(value="/index")
+	public ModelAndView homePage() {
+		return new ModelAndView("index","page","mainBody");
+	}
+	@RequestMapping(value="/contact")
+	public ModelAndView contactPage() {
+		return new ModelAndView("index","page","contact");
+	}
+	/* Koju's Portion Ends */
+	
+	/* Unika's Portion Ends */
+	/* Unika's Portion Ends */
+	
+	/* Sandesh's Portion Ends */
+	/* Sandesh's Portion Ends */
+	
+	//Request Mapping for Main Interface Ends
+	
+	//Request Mapping for User Interface Starts
+	
+	/* Koju's Portion Ends */
+	/* Koju's Portion Ends */
+	
+	/* Unika's Portion Ends */
+	/* Unika's Portion Ends */
+	
+	/* Sandesh's Portion Ends */
+	/* Sandesh's Portion Ends */
+	
+	//Request Mapping for User Interface Ends
+	
+	//Request Mapping for Admin Pannel Starts
+	
+	/* Koju's Portion Ends */
+	/* Koju's Portion Ends */
+	
+	/* Unika's Portion Ends */
+	/* Unika's Portion Ends */
+	
+	/* Sandesh's Portion Ends */
+	/* Sandesh's Portion Ends */
+	
+	//Request Mapping for Admin Pannel Ends
+}
